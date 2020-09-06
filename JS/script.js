5c4ddc3fb8c56607b8fb0621fa6f50d0621db52a
Michael Whitaker <miwhitaker@ymail.com>
2020-09-06 18:51:57 -0500

    removed unnecessary code

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,10 @@
 function handleClick () {
-    console.log(
-    parseInt(document.getElementById('systemsmenu').value));
+    
+    parseInt(document.getElementById('systemsmenu').value);
 
     var result = parseInt(document.getElementById('systemsmenu').value);
     var genre = document.getElementById('genresmenu').value;
 
-    console.log(result);
 
     if (result === 100) {location.assign("xb_all.html?genre=" + genre)}
     else if (result === 200) {location.assign("ps4_all.html?genre=" + genre)}
